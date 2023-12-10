1f5ec3f895c2cccd48e204d55e1f24588eaa28a5
niu0217 <1033856427@qq.com>
2023-12-10 14:55:49 +0800

    完成Django入门学习

--- Dev/learning_log/ll_project/urls.py
+++ Dev/learning_log/ll_project/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+# urlpatterns包含项目中应用程序的URL
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('learning_logs.urls'))
